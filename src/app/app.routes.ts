afa0ae97b521e0122e80ef1c57b238cda446a507
Dahiorus <brondon.ung@gmail.com>
2023-12-22 13:50:32 +0100

    Core: prepare project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'vending-machines',
+    loadChildren: () => import('./vending-machine/vending-machine.routes'),
+  },
+];
