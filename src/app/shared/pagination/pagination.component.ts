dea3d3ff025d21396987c330dcc95a2f91a97383
Dahiorus <brondon.ung@gmail.com>
2025-05-14 00:32:00 +0200

    refactor: Use Signals in pagination component

--- src/app/shared/pagination/pagination.component.ts
+++ src/app/shared/pagination/pagination.component.ts
@@ -4,27 +4,36 @@ import {
   LocationStrategy,
   PathLocationStrategy,
 } from '@angular/common';
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
 import { Link, Links } from '@model/entity.model';
 import { PageInfo } from '@model/page.model';
 
 @Component({
-    selector: 'app-pagination',
-    imports: [CommonModule],
-    providers: [
-        Location,
-        { provide: LocationStrategy, useClass: PathLocationStrategy },
-    ],
-    templateUrl: './pagination.component.html',
-    styleUrl: './pagination.component.css'
+  selector: 'app-pagination',
+  imports: [CommonModule],
+  providers: [
+    Location,
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+  ],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css',
 })
 export class PaginationComponent {
-  private location: Location = inject(Location);
+  private readonly location: Location = inject(Location);
 
-  @Input() baseUrl!: string;
-  @Input() pageInfo!: PageInfo;
-  @Input() pageLinks!: Links;
-  @Output() pageChanged = new EventEmitter<Link>();
+  baseUrl = input.required<string>();
+  pageInfo = input.required<PageInfo>();
+  pageLinks = input.required<Links>();
+
+  pageChanged = output<Link>();
+
+  pageNumber = computed(() => this.pageInfo().number + 1);
+  firstPage = computed(() => this.pageLinks()['first']);
+  previousPage = computed(() => this.pageLinks()['prev']);
+  nextPage = computed(() => this.pageLinks()['next']);
+  lastPage = computed(() => this.pageLinks()['last']);
+
+  constructor() {}
 
   changePage(link: Link): void {
     this.pageChanged.emit(link);
@@ -33,6 +42,6 @@ export class PaginationComponent {
 
   private updateLocation(link: Link) {
     const url = new URL(link.href);
-    this.location.go(this.baseUrl, url.search);
+    this.location.go(this.baseUrl(), url.search);
   }
 }
