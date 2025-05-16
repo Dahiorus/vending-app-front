bb20a2f7be124f7d4be06bfa678051747def0e7e
Dahiorus <brondon.ung@gmail.com>
2025-05-17 01:31:05 +0200

    (chore) Delete unused components

--- src/app/vending-machine/vending-machine.routes.ts
+++ src/app/vending-machine/vending-machine.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { ListMachineComponent } from './list-machine/list-machine.component';
-import { AddMachineComponent } from './add-machine/add-machine.component';
 import { ViewMachineComponent } from './view-machine/view-machine.component';
-import { EditMachineComponent } from './edit-machine/edit-machine.component';
 
 export default [
   {
@@ -10,15 +8,7 @@ export default [
     component: ListMachineComponent,
   },
   {
-    path: 'new',
-    component: AddMachineComponent,
-  },
-  {
     path: ':id',
     component: ViewMachineComponent,
-  },
-  {
-    path: ':id/edit',
-    component: EditMachineComponent,
   },
 ] satisfies Routes;
